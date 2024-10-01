cc92412fc0eeafc35bffc002b2ea8a9cc7dea1c1
Gemechu <95497037+Gesifeta@users.noreply.github.com>
2024-10-01 13:51:51 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,179 @@
+const data = {
+    "countries": [
+        {
+            "id": 1,
+            "name": "Australia",
+            "cities": [
+                {
+                    "name": "Sydney, Australia",
+                    "imageUrl": "https://images.unsplash.com/photo-1624138784614-87fd1b6528f8?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8c3lkbmV5JTIwY2l0eXxlbnwwfHwwfHx8MA%3D%3D",
+                    "description": "A vibrant city known for its iconic landmarks like the Sydney Opera House and Sydney Harbour Bridge."
+                },
+                {
+                    "name": "Melbourne, Australia",
+                    "imageUrl": "https://cdn.pixabay.com/photo/2017/11/29/14/54/melbourne-2986345_640.jpg",
+                    "description": "A cultural hub famous for its art, food, and diverse neighborhoods."
+                }
+            ]
+        },
+        {
+            "id": 2,
+            "name": "Japan",
+            "cities": [
+                {
+                    "name": "Tokyo, Japan",
+                    "imageUrl": "https://media.istockphoto.com/id/904453184/photo/composite-image-of-mt-fuji-and-tokyo-skyline.jpg?b=1&s=612x612&w=0&k=20&c=tjQ_IG4xMOgrNfvOoigLe8AMkwMYhIE3ab4e5UDT_eg=",
+                    "description": "A bustling metropolis blending tradition and modernity, famous for its cherry blossoms and rich culture."
+                },
+                {
+                    "name": "Kyoto, Japan",
+                    "imageUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwyd0UxYaX_wvTc8N38uiSHvvVdjM4j9GddQ&s",
+                    "description": "Known for its historic temples, gardens, and traditional tea houses."
+                }
+            ]
+        },
+        {
+            "id": 3,
+            "name": "Brazil",
+            "cities": [
+                {
+                    "name": "Rio de Janeiro, Brazil",
+                    "imageUrl": "https://images.pexels.com/photos/1804177/pexels-photo-1804177.jpeg?auto=compress&cs=tinysrgb&w=600",
+                    "description": "A lively city known for its stunning beaches, vibrant carnival celebrations, and iconic landmarks."
+                },
+                {
+                    "name": "São Paulo, Brazil",
+                    "imageUrl": "https://cdn.pixabay.com/photo/2022/03/07/17/47/paulista-avenue-7054178_640.jpg",
+                    "description": "The financial hub with diverse culture, arts, and a vibrant nightlife."
+                }
+            ]
+        }
+    ],
+    "temples": [
+        {
+            "id": 1,
+            "name": "Angkor Wat, Cambodia",
+            "imageUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRw36Ve5JhK3jmwzxv7A2bDUdic3OatvsYnhw&s",
+            "description": "A UNESCO World Heritage site and the largest religious monument in the world."
+        },
+        {
+            "id": 2,
+            "name": "Taj Mahal, India",
+            "imageUrl": "enter_your_image_for_taj-mahal.jpg",
+            "description": "An iconic symbol of love and a masterpiece of Mughal architecture."
+        }
+    ],
+    "beaches": [
+        {
+            "id": 1,
+            "name": "Bora Bora, French Polynesia",
+            "imageUrl": "https://cdn.pixabay.com/photo/2017/12/16/22/22/bora-bora-3023437_1280.jpg",
+            "description": "An island known for its stunning turquoise waters and luxurious overwater bungalows."
+        },
+        {
+            "id": 2,
+            "name": "Copacabana Beach, Brazil",
+            "imageUrl": "https://cdn.pixabay.com/photo/2021/02/02/20/40/beach-5975314_640.jpg",
+            "description": "A famous beach in Rio de Janeiro, Brazil, with a vibrant atmosphere and scenic views."
+        }
+    ]
+}
+async function fetchData() {
+
+    let URI = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMSkillsNetwork-JS0101EN-SkillsNetwork/travel1.json"
+    const destination = fetch(URI)
+        .then(response => response.json())
+        .then(data => data)
+        .catch(error => console.error('Error fetching data:', error));
+
+    return await destination;
+}
+
+window.addEventListener('load', fetchData);
+
+function selectNavLink(e) {
+    console.log("clicked")
+    const navLinks = document.querySelectorAll("a")
+    navLinks.forEach((link, index) => {
+        link.parentNode.classList.remove("active");
+    })
+    // e.target.partentNode.classList.add("active")
+}
+function hideSearchBar() {
+    const searchBar = document.getElementById("searchBar")
+    searchBar.style.display = "none"
+}
+
+async function searchDestinations(event) {
+    const { countries, temples, beaches } = data;
+
+    let searchString = event.target.value.toLowerCase().trim()
+    if (searchString.length < 1) return
+
+    let isCountrySearch = countries.some(country => country.name.toLowerCase().indexOf(searchString) > -1)
+    let isTempleSearch = temples.some(temple => temple.name.toLowerCase().indexOf(searchString) > -1)
+    let isBeachSearch = beaches.some(beach => beach.name.toLowerCase().indexOf(searchString) > -1)
+
+    if (isCountrySearch) {
+        let result = countries.filter(country => country)
+
+        result.map(country => displayCards(country)
+
+        )
+    }
+    else if (isTempleSearch) {
+        let result = temples.filter(temple => temple)
+
+        result.map(temple => displayCards(temple)
+
+        )
+    }
+    else if (isBeachSearch) {
+        let beaches = beaches.filter(beach => beach)
+        beaches.map(beach => displayCards(beach)
+
+        )
+    }
+
+}
+function displayAllCountries(event) {
+
+    const { countries } = data;
+    countries.map(country => displayCards(country, event))
+}
+//display cards
+function displayCards(data, event) {
+    const cardContainer = document.getElementById("app__destination-card-container")
+
+        cardContainer.innerHTML = ""
+       
+    if(event){
+        event.preventDefault()
+        cardContainer.style.display="flex"
+    }
+    const destinationCard = document.createElement("div")
+    cardContainer.addEventListener("mouseleave", (e)=>cardContainer.style.display="none")
+    destinationCard.classList.add("app__destination-card")
+    const imageContainer = document.createElement("div")
+    imageContainer.classList.add("app__destination-card--image")
+    const cardImage = document.createElement("img")
+    const visit= document.createElement("a")
+    visit.setAttribute("target", "_blank")
+    visit.setAttribute("rel", "noopener noreferrer")
+    visit.setAttribute("href", `${data.imageUrl ? data.imageUrl : data.cities[0].imageUrl}`)
+    visit.classList.add("btn-primary")
+    visit.innerText="Visit"
+    cardImage.setAttribute("alt", data.name)
+    cardImage.setAttribute("src", data.imageUrl ? data.imageUrl : data.cities[0].imageUrl)
+    const cardTitle = document.createElement("h2")
+    cardTitle.innerText = data.name
+    const cardDescription = document.createElement("p")
+    cardDescription.innerText = data.description ? data.description : data.cities[0].description;
+    cardContainer.appendChild(destinationCard)
+    destinationCard.appendChild(imageContainer)
+    imageContainer.appendChild(cardImage)
+    destinationCard.appendChild(cardTitle)
+    destinationCard.appendChild(cardDescription)
+    destinationCard.appendChild(visit)
+    return destinationCard;
+}
